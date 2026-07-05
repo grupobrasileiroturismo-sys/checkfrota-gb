72b45fdc4d64ad7e5bd11d2f6bfcb938d4e45588
grupobrasileiroturismo-sys <grupobrasileiroturismo@gmail.com>
2026-07-04 23:26:47 -0300

    V2.4

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 // API
 //========================================
 
-const URL_WEBAPP = "https://script.google.com/macros/s/AKfycbxiuIzQJ3UQE-VE1WThPYeulizVgbi-2huh74xKkjX2r0pkcinD5c4s3nv24KdFAtqb/exec";
+const URL_WEBAPP = "https://script.google.com/macros/s/AKfycbwsLRinkLd7vByC647VWo4MU3T1yF3pakpX3BabPq0h_8LtxboE6byDFQ0eP9zA09if/exec";
 
 
 //========================================
